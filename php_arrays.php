07ce39bf576c8722c5b311bccc1f3a9183f02e9e
dushyant basson <39452934+onizu-db@users.noreply.github.com>
2019-01-09 08:59:18 +0530

    Create php_arrays.php
    an example of creating and adding values to arrays in different ways.. quite useful ready reference

---
+++ php_arrays.php
@@ -0,0 +1,20 @@
+<?php
+
+$array = []; // Create empty array
+
+var_dump($array); 
+echo "-----\n";
+
+$array = ['abc']; // Assign the value ['abc'] (array with one element 0 => 'abc' to $array)
+
+var_dump($array);
+echo "-----\n";
+
+$array[] = 'abc'; // Add a new element to the array 'abc'
+
+var_dump($array);
+echo "-----\n";
+
+$array += [0 => 'NEWabc', 4 => 'anotherABC']; // Concats two arrays, but the key [0] is already existing in the left-handside array so it ignores the first value from the right hand side array and only adds the second element (key 4 is not existing)
+
+var_dump($array);
