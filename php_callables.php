496c43307782c57a255f464a5add4df83d41e096
dushyant basson <39452934+onizu-db@users.noreply.github.com>
2020-05-13 23:50:47 +0530

    Update php_callables.php

--- php_callables.php
+++ php_callables.php
@@ -16,3 +16,22 @@ function func2($funcName, $arg, $funcName2, $arg2A, $arg2B) {
 
 $result = func2('func1', 'hi', 'sum', 2, 3);
 echo "\n".$result;
+
+// In a class:
+
+class Test {
+    private function func1($arg1) {
+        echo "\n".$arg1;
+    }
+    
+    private function func2($funcName, $arg) {
+        call_user_func($funcName, $arg);
+    }
+    
+    public function func3() {
+        $this->func2('func1', 'hello');
+    }
+}
+
+$a = new Test();
+$a->func3();
